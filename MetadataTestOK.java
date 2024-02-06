b68266ee02d05ce2bd3aea5cdf822c1aea7fddff
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2024-02-06 22:20:41 +0100

    Create MetadataTestOK.java

---
+++ MetadataTestOK.java
@@ -0,0 +1,21 @@
+package junit.metadata;
+
+import org.junit.jupiter.api.Assertions;
+
+public class MetadataTestOK {
+
+    @Test
+    @TFMetadata ( key = "first_KEY", value = {"first_VALUE"})
+    public void keyValue(){
+        int a = 2;
+        int b = 2;
+        Assertions.assertEquals(a, b);
+    }
+
+    @Test
+    @TFMetadata ( key = "solo_KEY")
+    public void keyOnly(){
+        int a = 2;
+        int b = 2;
+        Assertions.assertEquals(a, b);
+    }
